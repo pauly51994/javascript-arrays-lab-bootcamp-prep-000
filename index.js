cb209f4e5ae1dd35cc9091abcaf6db362fea64e8
Paul Yun <p.yun1994@gmail.com>
2019-06-21 20:29:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ var kittens = ['Milo', 'Otis', 'Garfield']; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){return kittens.push(name)}
+function destructivelyPrependKitten(name){return kittens.unshift(name)}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){}
