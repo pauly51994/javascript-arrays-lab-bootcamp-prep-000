87bbb62ed67167609d7e95b16981eb4437f2a786
Paul Yun <p.yun1994@gmail.com>
2019-06-21 20:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function appendKitten(name){return [...kittens, name]}
 
 function prependKitten(name){return [name, ...kittens]}
 
-function removeLastKitten(){return kittens.slice(1)}
+function removeLastKitten(){return kittens.slice(0, -1)}
 
-function removeFirstKitten(){return kittens.slice(0,-1)}
+function removeFirstKitten(){return kittens.slice(1)}
